eb44eb5da5c141c12f852bc6ca75dc5c2b488a67
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-03-21 12:23:10 +0200

    Adding somen functions in the class MailList

--- mailList.py
+++ mailList.py
@@ -6,3 +6,13 @@ class MailList():
 
     def getName(self):
         return self.__name
+
+    def getSubscribers(self):
+        return self.subscribers
+
+    def add_subscriber(self, name, mail):
+        self.subscribers[name] = mail
+        return self.subscribers
+
+if __name__ == '__main__':
+    main()
