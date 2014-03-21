03f63c0170bdf8729fecabc03e2c41561eec96b4
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-03-21 13:29:08 +0200

    New tests for mailList

--- test_mailList.py
+++ test_mailList.py
@@ -7,16 +7,19 @@ class TestMailList(unittest.TestCase):
     def setUp(self):
         self.obj = MailList("someName")
         self.obj.add_subscriber("Pepa", "pepa@pepa")
-        self.obj.add_subscriber("Tsveta", "tsveta@tsveta")
 
     def test_atributes(self):
         self.assertEqual("someName", self.obj.getName())
 
     def test_add_subscriber(self):
-        _dict = {}
-        _dict["Pepa"] = "pepa@pepa"
-        _dict["Tsveta"] = "tsveta@tsveta"
-        self.assertEqual(_dict, self.obj.getSubscribers())
+        self.obj.add_subscriber("Tsveta", "tsveta@tsveta")
+        expected = [("Pepa", "pepa@pepa"), ("Tsveta", "tsveta@tsveta")]
+        self.assertEqual(expected, self.obj.get_subscribers())
+
+    def test_get_subscribers(self):
+        self.obj.add_subscriber("Tsveta", "tsveta@tsveta")
+        expected = [("Pepa", "pepa@pepa"), ("Tsveta", "tsveta@tsveta")]
+        self.assertEqual(expected, self.obj.get_subscribers())
 
 
 if __name__ == '__main__':
